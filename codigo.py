5f586d9d6bc0645d69c3d93bb1f3a5e1da4ac2d6
Oscar Fabian Archila Cruz <of.archila10@uniandes.edu.co>
2020-02-07 13:41:56 -0500

    codigo color y letura de imagenes

--- codigo.py
+++ codigo.py
@@ -1,5 +1,23 @@
-import numpy as np
-import cv2
+import scipy.io as sio
+import matplotlib.pyplot as plt
+import imageio
 
-print(15+20)
-print('hello')
+img = 'BSDS_small/train/22090.jpg'
+plt.imshow(imageio.imread(img))
+plt.show()
+
+# Load .mat
+gt=sio.loadmat(img.replace('jpg', 'mat'))
+
+#Load segmentation from sixth human
+segm=gt['groundTruth'][0,5][0][0]['Segmentation']
+plt.imshow(segm, cmap=plt.get_cmap('hot'))
+plt.colorbar()
+plt.show()
+print(segm)
+
+#Boundaries from third human
+segm=gt['groundTruth'][0,2][0][0]['Boundaries']
+plt.imshow(segm)
+plt.colorbar()
+plt.show()
